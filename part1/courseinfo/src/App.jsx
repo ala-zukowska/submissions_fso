1e3970d5bc09bbb2e83d41faa88d7d600750f1b6
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2024-06-03 18:07:50 +0300

    Part 1: 1.1 Refactor the code so that it consists of three new components: Header, Content, and Total

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,26 @@
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name}: {props.number}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.number}</p>
+    </div>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -10,17 +33,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+      <Content name={part1} number={exercises1} />
+      <Content name={part2} number={exercises2} />
+      <Content name={part3} number={exercises3} />
+      <Total number={exercises1+exercises2+exercises3} />
     </div>
   )
 }
