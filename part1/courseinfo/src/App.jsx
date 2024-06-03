906ecb2406a027f111632379396c72fae3e57107
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2024-06-03 20:44:11 +0300

    1.3 Use objects in the App

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,18 +34,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content namelist={[part1,part2,part3]} numberlist={[exercises1,exercises2,exercises3]} />
-      <Total number={exercises1+exercises2+exercises3} />
+      <Content namelist={[part1.name,part2.name,part3.name]} numberlist={[part1.exercises,part2.exercises,part3.exercises]} />
+      <Total number={part1.exercises+part2.exercises+part3.exercises} />
     </div>
   )
 }
