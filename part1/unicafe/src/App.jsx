0e641b2d5a3399d273f158bb006a9656b3e31ca7
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2025-09-17 21:40:59 +0300

    1.8 Refactor the code to have a Statistics component. Replace the unnecessary total and avrSum states with calculations.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,43 +4,33 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Display = ({ text, counter, textAfter = "" }) => <div>{text} {counter}{textAfter}</div>
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [avrSum, setAvrSum] = useState(0)
-
-  const handleSetGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
-    setAvrSum(avrSum + 1)
-  }
-
-  const handleSetBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-    setAvrSum(avrSum - 1)
-  }
-
-  const handleSetNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-  }
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick={handleSetGood} text="good"/>
-      <Button onClick={handleSetNeutral} text="neutral"/>
-      <Button onClick={handleSetBad} text="bad"/>
-      <h1>statistics</h1>
+const Statistics = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const avrSum = good - bad
+  return <div>
       <Display text="good" counter={good}/>
       <Display text="neutral" counter={neutral}/>
       <Display text="bad" counter={bad}/>
       <Display text="all" counter={total}/>
       <Display text="average" counter={avrSum/total}/>
       <Display text="positive" counter={(good/total)*100} textAfter=' %'/>
+  </div>
+
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
+      <h1>statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
