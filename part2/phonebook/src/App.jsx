766a8b493fb7bcdd64c7b31109d5b0daf3f3470e
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2025-09-20 16:18:47 +0300

    2.9 Implement the filter field

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
 
 const NumbersDisplay = ({ persons }) => {
-  return persons.map((person) => 
+  return persons.map(person => 
     <p key={person.name}>{person.name} {person.number}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.some( (person) => person.name === newName)) {
+    if (persons.some(person => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`)
     }    
     else {
@@ -38,10 +40,25 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const personsToDisplay = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input
+            value={newFilter}
+            onChange={handleFilterChange}
+            />
+        </div>
+      </form>
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -59,7 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <NumbersDisplay persons={persons}/>
+      <NumbersDisplay persons={personsToDisplay}/>
     </div>
   )
 }
