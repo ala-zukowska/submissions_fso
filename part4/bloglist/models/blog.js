649ceec33f101836934151742e6bdb4d807d6657
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2026-01-13 01:28:18 +0200

    4.1 Create functioning Blog LIst npm project

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl, { family: 4 })
+    .then( () => {
+        console.log('Connected to MongoDB')
+    })
+    .catch( (error) => {
+        console.log(`Error connecting to MongoDB: ${error.message}`)
+    })
+
+mongoose.set('setDefaultsOnInsert', false);
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Blog title is required!'],
+    unique: true,
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: [true, 'Blog author is required!'],
+    unique: true,
+    trim: true,
+  },
+  url: {
+    type: String,
+    required: [true, 'Blog url is required!'],
+    unique: true,
+    trim: true,
+    validate: {
+      validator: function(value) {
+        return /^https?:\/\/[^\s\/]+[^\s]*$/.test(value) //a *very very* simple url validation//
+      },
+      message: props => `${props.value} is not a valid url!`
+    }
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnObject) => {
+        returnObject.id = returnObject._id.toString()
+        delete returnObject._id
+        delete returnObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
