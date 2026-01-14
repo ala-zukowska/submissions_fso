c7f3e17d107e10084420b7bb47814f74e3e13187
ala-zukowska <132846952+ala-zukowska@users.noreply.github.com>
2026-01-14 18:36:22 +0200

    4.4 Define totalLikes function and test it

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs_list) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+
+    return blogs_list.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
